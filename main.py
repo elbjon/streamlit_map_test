28c85f8810290d731beb4839032c3d16953faa6d
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-29 16:05:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,28 @@ from streamlit_folium import st_folium, folium_static
 
 
 
+import folium 
+from streamlit_folium import st_folium
+import streamlit as st
 
+
+def get_pos(lat, lng):
+    return lat, lng
+
+
+m = folium.Map()
+
+m.add_child(folium.LatLngPopup())
+
+map = st_folium(m, height=350, width=700)
+
+data = None
+if map.get("last_clicked"):
+    data = get_pos(map["last_clicked"]["lat"], map["last_clicked"]["lng"])
+
+if data is not None:
+    st.write(data) # Writes to the app
+    print(data) # Writes to terminal
 
 
 
