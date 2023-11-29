96e02d130737c85b9078878248c5550b4bdc176e
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-29 23:48:40 +0100

    g

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium, folium_static
 from folium.plugins import ImageOverlay
+from folium.plugins import ImageOverlay
+from io import BytesIO
+from PIL import Image
 
 
 # Create a map using the Map() function and the coordinates for Boulder, CO
@@ -89,18 +92,31 @@ folium_static(m)
 
 
 
-#if not os.path.isfile(merc):
-#    print(f"Could not find {merc}")
-#else:
-img = folium.raster_layers.ImageOverlay(
-    name="Map Overlay",
-    image='1.png',
+
+# Create a Folium map centered at a specific location
+m = folium.Map(location=[52, 10], zoom_start=10)
+
+# Load your image
+image_path = "1.png"
+img = Image.open(image_path)
+
+# Convert the image to BytesIO object
+img_byte_array = BytesIO()
+img.save(img_byte_array, format='PNG')
+
+# Create an ImageOverlay
+image_overlay = ImageOverlay(
+    img_byte_array.getvalue(),
     bounds=[[52, 10], [53, 11]],
-    #opacity=0.6,
-    #interactive=True,
-    #cross_origin=False,
-    #zindex=1,
+    opacity=0.6,
 )
+
+# Add the ImageOverlay to the Folium map
+image_overlay.add_to(m)
+
+# Display the map in Streamlit
+st.markdown(m._repr_html_(), unsafe_allow_html=True)
+
 
 
 #folium.LayerControl().add_to(m)
