15f4e3cea5b622c56bf681cdd58007f05fd915aa
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-29 15:58:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,13 @@ from streamlit_folium import st_folium, folium_static
 
 
 
-
+f = folium.Figure(width=1000, height=500)
+m = folium.Map(location= [52, 10], 
+           tiles="openstreetmap",
+           zoom_start=10, 
+           min_zoom = 6,
+           max_bounds = True 
+           ).add_to(f)
 
 
 
@@ -26,11 +32,11 @@ from streamlit_folium import st_folium, folium_static
 
 
 # center on Liberty Bell, add marker
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+x = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
 folium.Marker(
     [39.949610, -75.150282], popup="HELLO", tooltip="HELLOHELLO"
-).add_to(m)
+).add_to(x)
 
 # call to render Folium map in Streamlit
-folium_static(m)
+folium_static(x)
 #st_folium(m, width=725)
